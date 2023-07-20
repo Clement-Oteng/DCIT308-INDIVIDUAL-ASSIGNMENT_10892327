72594623dc4e553864170ded9b38b5fd04b385e3
Clement-Oteng <92687376+Clement-Oteng@users.noreply.github.com>
2023-07-20 12:07:33 +0000

    Update Main.java

--- DCIT308_INDIVIDUAL PROJECT_10892327/src/main/Main.java
+++ DCIT308_INDIVIDUAL PROJECT_10892327/src/main/Main.java
@@ -11,7 +11,7 @@ import javafx.stage.Stage;
 public class Main extends Application {
     public static Management inventory;
 
-    @Override
+    
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../fxml/products.fxml"));
         primaryStage.setTitle("");
